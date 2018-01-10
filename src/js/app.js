8f9c4332a4589868e4d02660d2290450e43035a4
Alexander Fedorov <hamster2240@gmail.com>
2018-01-10 06:23:54 +0300

    add xScale and yScale

--- src/js/app.js
+++ src/js/app.js
@@ -5,18 +5,22 @@ d3.csv("../data/russia-people-density.csv", function(data) {
 
   var w = 1200;
   var h = 600;
-  var barPadding = 1;
- // var maxValue = ;
- var min = d3.min(data, function(d) { return +d.value;} );
-// console.log(maxValue)
-console.log(min)
+  // var barPadding = 1;
+
   var svg = d3.select(".barchart")
               .append("svg")
               .attr("width", w)
               .attr("height", h)
-var scaleColor = d3.scaleLinear()
-              .domain([0, d3.max(data, function(d) { return +d.value;} )])
-              .range([0, 255])
+  var scaleColor = d3.scaleLinear()
+                .domain([0, d3.max(data, function(d) { return +d.value;} )])
+                .range([0, 255])
+  var xScale = d3.scaleBand()
+          .domain(d3.range(dataset.length))
+          .rangeRound([0, w])
+          .paddingInner(0.05);
+  var yScale = d3.scaleLinear()
+                .domain([0, d3.max(data, function(d) { return +d.value;} )])
+                .range([0, h])
 
   svg.selectAll("rect")
       .data(dataset)
@@ -26,15 +30,28 @@ var scaleColor = d3.scaleLinear()
         return i * (w / dataset.length);
       })
       .attr("y", function(d) {
-        return h - (d.value * 0.00002);
+        j = d.value
+        return h - yScale(j);
       })
-      .attr("width", w / dataset.length - barPadding)
+      .attr("width", xScale.bandwidth())
       .attr("height", function(d) {
-        return d.value * 0.00002
+        j = d.value
+        return yScale(j)
       })
       .attr("fill", function(d) {
         j = d.value
-        console.log("rgb(" + Math.round(scaleColor(j)) + ", 0, 0)")
-       return "rgb(" + Math.round(scaleColor(j)) + ",255, 0)";
+        // console.log("rgb(" + Math.round(scaleColor(j)) + ", 0, 0)")
+       return "rgb(" + Math.round(scaleColor(j)) + ",155, 155)";
       });
+
+      // labels
+  svg.selectAll("text")
+    .data(dataset)
+    .text(function(d) {
+      k = d.region
+      return k
+    })
+    .attr("x", function(d,i) {
+
+    })
 });
