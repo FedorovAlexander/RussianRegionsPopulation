f69ebad19f0999fcc434baddcf4924536d2db98a
Alexander Fedorov <hamster2240@gmail.com>
2018-01-29 06:46:28 +0300

    add the moving tooltips to the bubbles

--- src/js/script.js
+++ src/js/script.js
@@ -16,14 +16,13 @@ var svgBub = d3.select(".bubble-chart")
 
 var formatBub = d3.format(",d");
 
-var colorBub = d3.scaleOrdinal(d3.schemeCategory20c);
+var colorBub = d3.scaleOrdinal(d3.schemeCategory10);
 
 var pack = d3.pack()
     .size([width, height])
     .padding(1.5);
 
 d3.csv("../data/russia-people-density.csv", function(d) {
-  d.value = +d.value;
   if (d.value) return d;
 }, function(error, classes) {
   if (error) throw error;
@@ -46,7 +45,7 @@ d3.csv("../data/russia-people-density.csv", function(d) {
       .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
   node.append("circle")
-      .attr("id", function(d) { return d.region; })
+      .attr("class", function(d) { return "bubble " + d.data.region; })
       .attr("r", function(d) { return d.r; })
       .style("fill", function(d) { return colorBub(d.package); });
 
@@ -55,6 +54,41 @@ d3.csv("../data/russia-people-density.csv", function(d) {
     .append("use")
       .attr("xlink:href", function(d) { return "#" + d.data.region; });
 
-  node.append("title")
-      .text(function(d) { return d.data.region + "\n" + formatBub(d.value); });
+  svgBub.selectAll(".bubble")
+    .on("mousemove", function(d) {
+
+      var xPosition = d3.event.pageX;
+      var yPosition = d3.event.pageY;
+
+      // Make units correct
+      function tooltipText(j) {
+        j = d.value;
+        if (j % 10 > 4 || j % 10 <= 1) {
+          return j + " человек"
+        }
+        else {
+          return j + " человека"
+        }
+      }
+      //Update the tooltip position, value and label
+      d3.select(".tooltip-bub")
+        .style("left", xPosition + "px")
+        .style("top", yPosition + "px")
+        .select(".tooltip-bub__text--value")
+        .text(tooltipText())
+      d3.select(".tooltip-bub")
+        .select(".tooltip-bub__text--label")
+        .text(d.data.region);
+
+      // Show the tooltip
+      d3.select(".tooltip-bub")
+      .classed("hidden", false)
+      .transition(500)
+    })
+
+  svgBub.selectAll(".bubble")
+      // Hide the tooltip
+      .on("mouseout", function() {
+        d3.select(".tooltip-bub").classed("hidden", true);
+      })
 });
